842a509f61a4983e93ca38ac5411faee4fd29548
DenisFulks <denisfulks@gmail.com>
2026-09-08 13:25:13 -0400

    First Commit

---
+++ Daily Quizes/0902026/check5.m
@@ -0,0 +1,11 @@
+x = 2;
+
+if x < 5
+    x = -1;
+elseif x > 5
+    x = 1;
+else
+    x = 0;
+end
+
+fprintf("%0.2f\n", x)
---
+++ Daily Quizes/0902026/sillyMonkey.m
@@ -0,0 +1,7 @@
+function [] = sillyMonkey()
+    a = valueDoubler(4)
+end
+
+function [doubleTheValue] = valueDoubler(value)
+    doubleTheValue = 2*value;
+end
