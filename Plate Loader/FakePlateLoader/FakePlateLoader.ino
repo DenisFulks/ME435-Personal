44f27d89767ae4bec5447639cd230e252898b3ec
DenisFulks <denisfulks@gmail.com>
2026-09-14 13:47:53 -0400

    Finished Fake Plate Loader

--- Plate Loader/FakePlateLoader/FakePlateLoader.ino
+++ Plate Loader/FakePlateLoader/FakePlateLoader.ino
@@ -12,9 +12,22 @@ void loop() {
   if (isStringComplete) {
     if (inputString.equals("RESET")) {
       Serial.println("READY, SAGIAN PE LOADER, ROM VER. 1.1.6, 12APR2001");
-    } else if (inputString.equals("X_AXIS")) {
-      digitalWrite(LED_BUILTIN, LOW);
-      Serial.println("The LED is now off");
+    } else if (inputString.startsWith("X-AXIS")) {
+      Serial.println("READY, SAGIAN PE LOADER, ROM VER. 1.1.6, 12APR2001");
+    } else if (inputString.startsWith("Z-AXIS EXTEND")) {
+      Serial.println("READY, EXTENDED");
+    } else if (inputString.startsWith("Z-AXIS RETRACT")) {
+      Serial.println("READY, RETRACTED");
+    } else if (inputString.equals("GRIPPER OPEN")) {
+      Serial.println("READY, OPEN");
+    } else if (inputString.equals("GRIPPER CLOSE")) {
+      Serial.println("READY, CLOSED, NOPLATE");
+    } else if (inputString.startsWith("MOVE")) {
+      Serial.println("READY");
+    } else if (inputString.equals("LOADER_STATUS")) {
+      Serial.println("READY");
+    } else if (inputString.startsWith("SET_DELAY")) {
+      Serial.println("READY");
     } else {
       Serial.print("Unrecognized Command --> ");
       Serial.println(inputString);
