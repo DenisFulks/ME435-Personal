3ae07d28d19eda7b7fe516c1f2a395d73ad71c49
DenisFulks <denisfulks@gmail.com>
2026-09-21 13:57:46 -0400

    Serial Progress

---
+++ Lab3/initial_pyserial_learning.py
@@ -0,0 +1,15 @@
+import serial
+
+print("Learning Pyserial")
+
+ser = serial.Serial(port='/dev/ttyACM0', baudrate=19200, timeout=10)
+
+while not ser.is_open:
+    print("Opening...")
+
+# TODO: use the ser obj
+
+ser.close()
+
+
+
